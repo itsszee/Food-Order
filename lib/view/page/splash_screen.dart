a69840fe4ed74fb75234cfaedaf771810de3105b
itsszee <bilqistyjazilarizki@gmail.com>
2024-11-13 14:30:38 +0700

    fixed

--- lib/view/page/splash_screen.dart
+++ lib/view/page/splash_screen.dart
@@ -52,7 +52,6 @@ class _SplashScreenState extends State<SplashScreen> {
               ),
             ),
             const SizedBox(height: 30),
-            // Indikator loading
             const CircularProgressIndicator(
               valueColor: AlwaysStoppedAnimation<Color>(Colors.orange),
             ),
